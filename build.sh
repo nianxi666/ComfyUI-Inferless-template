001e10bc69fb846d4a8ddb46add0b36f2baa7f91
nianxi4563 <62866097+nianxi666@users.noreply.github.com>
2025-05-29 11:40:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 comfy --skip-prompt --workspace=$NFS_VOLUME/ComfyUI install --nvidia
 pip install websocket-client
-pip install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
-pip install spaces
+/usr/bin/python3 -m pip install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
+pip3 install av
+pip3 install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
+pip3 install spaces
 pythoon --version
 comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors --relative-path models/unet --set-civitai-api-token $HF_ACCESS_TOKEN
 comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors --relative-path models/clip
