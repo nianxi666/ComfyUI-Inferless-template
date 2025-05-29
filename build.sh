c594c0815bf4e71975d628386636be4fae9c125c
nianxi4563 <62866097+nianxi666@users.noreply.github.com>
2025-05-29 13:12:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip3 install websocket-client
 /usr/bin/python3 -m pip install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
 pip3 install av
 pip3 install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
-pip3 install spaces
+pip3 install spaces websocket
 python --version
 comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors --relative-path models/unet --set-civitai-api-token $HF_ACCESS_TOKEN
 comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors --relative-path models/clip
