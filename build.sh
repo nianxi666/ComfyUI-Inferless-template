68f305a266a92a6d889503e0468755a2690884d0
nianxi4563 <62866097+nianxi666@users.noreply.github.com>
2025-05-29 14:10:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 comfy --skip-prompt --workspace=$NFS_VOLUME/ComfyUI install --nvidia
 /usr/bin/python3 -m pip install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
-pip3 install av
+pip3 install av websocket-client 
 pip3 install -r /var/nfs-mount/comfyui-storage/ComfyUI/requirements.txt
 pip3 install spaces
 python3 --version
